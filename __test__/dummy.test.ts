866a5cc16d7aa171d4c8eaa3395078de27fb766b
atorov <veselin.lkp@gmail.com>
2024-07-25 23:45:57 +0300

    updated tests

--- __test__/dummy.test.ts
+++
@@ -1,9 +0,0 @@
-// Added to experiment with the vitest setup
-
-import { describe, expect, test } from "vitest";
-
-describe("Just a dummy suite", () => {
-    test("Just a dummy test", () => {
-        expect(!false).not.toBeFalsy();
-    });
-});
